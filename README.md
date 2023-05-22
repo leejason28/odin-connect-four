4d8298f3e945266c2854e5722b64d4311b55520f
leejason28 <jason928lee@gmail.com>
2023-05-22 15:37:16 -0700

    basic skeleton

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # odin-connect-four
+
+odin project: connect four. build connect four with TDD
---
+++ lib/cfour.rb
@@ -0,0 +1,15 @@
+class Board 
+
+  def initialize
+    @grid = Array.new(6) { Array.new(7) }
+  end
+
+  def place
+
+  end
+
+end
+
+class Player
+  #player with first move is yellow
+end
