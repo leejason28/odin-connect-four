c25a22d4ae716f223c9063543b1ce883df45ea10
leejason28 <jason928lee@gmail.com>
2023-05-22 17:46:17 -0700

    implementing basic game logic

--- lib/cfour.rb
+++ lib/cfour.rb
@@ -1,10 +1,61 @@
-class Board 
+class Grid 
+
+  attr_accessor :grid
 
   def initialize
     @grid = Array.new(6) { Array.new(7) }
   end
 
-  def place
+  def place(player, column) #pieces "fall down" so need to go to max row (for loop from -1..0 ???)
+
+  end
+
+  def valid_move?(column) #not valid if all rows for this column are occupied
+    if column<0 || column>6
+      return false
+    else
+      if grid[0][column]
+        return false
+      end
+      return true
+    end
+  end
+
+  def game_over?                              # make sure to check that square actually has a checker on it (not nil)
+    if self.check_rows==true
+      return true
+    elsif self.check_columns==true
+      return true
+    elsif self.check_diagonals==true
+      return true
+    else
+      return false
+    end
+  end
+
+  def check_rows
+    for row in 0...@grid.length
+      for col in 0...@grid[0].length-3
+        if @grid[row][col]==@grid[row][col+1] && @grid[row][col]==@grid[row][col+2] && @grid[row][col]==@grid[row][col+3] && @grid[row][col]==@grid[row][col+4] #lmao
+          return true
+        end
+      end
+    end
+    return false
+  end
+
+  def check_columns
+    for row in 0...@grid.length-3
+      for col in 0...@grid[0].length
+        if @grid[row][col]==@grid[row+1][col] && @grid[row][col]==@grid[row+2][col] && @grid[row][col]==@grid[row+3][col] && @grid[row][col]==@grid[row+4][col] #lmao
+          return true
+        end
+      end
+    end
+    return false
+  end
+
+  def check_diagonals
 
   end
 
@@ -12,4 +63,11 @@ end
 
 class Player
   #player with first move is yellow
+
+    attr_accessor :id
+
+    def initialize
+      @id
+    end
+
 end
--- spec/cfour_spec.rb
+++ spec/cfour_spec.rb
@@ -1,1 +1,55 @@
 require './lib/cfour'
+
+describe Grid do
+
+  describe "grid size" do
+    it "should be a 6 row, 7 column grid" do
+      g = Grid.new
+      expect(g.grid.length).to eql(6)
+      expect(g.grid[0].length).to eql(7)
+    end
+  end
+
+  describe "valid moves" do
+    it "valid moves are between columns 0 and 6" do
+      g = Grid.new
+      expect(g.valid_move?(-1)).to eql(false)
+      expect(g.valid_move?(7)).to eql(false)
+      expect(g.valid_move?(4)).to eql(true)
+    end
+    it "you cannot place checkers on completely full column" do
+      g = Grid.new
+      for i in 0...g.grid.length
+        g.grid[i][0] = 'R'
+      end
+      expect(g.valid_move?(0)).to eql(false)
+      expect(g.valid_move?(1)).to eql(true)
+    end
+  end
+
+  describe "game over" do
+    it "check vertical" do
+      g = Grid.new
+      expect(g.game_over?).to eql(false)
+      for i in 0...4
+        g.grid[i][0] = 'R'
+      end
+      expect(g.game_over?).to eql(true)
+    end
+    it "check horizontal" do
+      g = Grid.new
+      for i in 0...4
+        g.grid[0][i] = 'R'
+      end
+      expect(g.game_over?).to eql(true)
+    end
+    it "check diagonal" do
+      g = Grid.new
+      for i in 0...4
+        g.grid[i][i] = 'R'
+      end
+      expect(g.game_over?).to eql(true)
+    end
+  end
+
+end
